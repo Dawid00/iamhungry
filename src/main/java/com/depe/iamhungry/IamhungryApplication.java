2d7b71d95c878abfed68db0957ff51d6cfad9fb2
dawid <dawidp789@gmail.com>
2022-04-10 00:34:22 +0200

    init

---
+++ src/main/java/com/depe/iamhungry/IamhungryApplication.java
@@ -0,0 +1,13 @@
+package com.depe.iamhungry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamhungryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamhungryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/depe/iamhungry/IamhungryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depe.iamhungry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamhungryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
