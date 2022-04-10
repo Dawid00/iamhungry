0763f7aba0400b3e6deb2a46fbfe4320a327d0f9
dawid <dawidp789@gmail.com>
2022-04-10 11:07:27 +0200

    change getRecipe method (change catch block)

--- src/main/java/com/depe/iamhungry/recipe/YummlyApiClient.java
+++ src/main/java/com/depe/iamhungry/recipe/YummlyApiClient.java
@@ -1,18 +1,15 @@
 package com.depe.iamhungry.recipe;
 
-
 import com.google.gson.Gson;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.ResponseBody;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 
 @Component
 public class YummlyApiClient {
-
 
     @Value("${yummly.api.url}")
     private String URL;
@@ -26,9 +23,7 @@ public class YummlyApiClient {
     private String API_HEADER_HOST_VALUE;
     private final OkHttpClient client = new OkHttpClient();
 
-
     public Root getRecipe(int startNumber) {
-        Root root;
         Request request = new Request.Builder()
                 .url(URL + "feeds/search?maxResult=1&start=" + startNumber)
                 .get()
@@ -38,11 +33,11 @@ public class YummlyApiClient {
         try {
             Gson gson = new Gson();
             ResponseBody responseBody = client.newCall(request).execute().body();
-            root = gson.fromJson(responseBody.string(), Root.class);
-        } catch (IOException ex) {
-            throw new RuntimeException();
+            return gson.fromJson(responseBody.string(), Root.class);
         }
-        return root;
+        catch (IOException ex) {
+            throw new ApiException();
+        }
     }
     public Root getRecipeWithName(String name) {
         Request request = new Request.Builder()
