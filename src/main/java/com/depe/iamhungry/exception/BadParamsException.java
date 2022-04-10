86b5893d295e5188cd0e603d240c271ca841b660
dawid <dawidp789@gmail.com>
2022-04-10 11:55:57 +0200

    change bad params exception message

--- src/main/java/com/depe/iamhungry/exception/BadParamsException.java
+++ src/main/java/com/depe/iamhungry/exception/BadParamsException.java
@@ -3,6 +3,6 @@ package com.depe.iamhungry.exception;
 public class BadParamsException extends RuntimeException {
 
     public BadParamsException() {
-        super("Query Params: name is required, limit and start are optional but limit + start must not be greater than 500");
+        super("query params: name is required, limit(must be greater than 0) and start(must be greater than -1) are optional but limit + start must not be greater than 500");
     }
 }
